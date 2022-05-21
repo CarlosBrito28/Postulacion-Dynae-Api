51bffff977da196e23e2c5111c38c96240a98585
Carlos <cdbm342@gmail.com>
2022-05-21 16:53:56 -0400

    Se crea la carpeta hooks la carpeta componets y algunos archivos dentro

---
+++ src/Components/Principal/CustomAPP.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './principal.css';
+
+
+export const CustomAPP = () => {
+  return (
+    <div>
+        <h1>Hola</h1>
+        <hr/>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CustomAPP } from './Components/Principal/CustomAPP';
  
+
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-   // < />
+    <CustomAPP />
 
 );
